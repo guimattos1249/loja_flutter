3fcaa74815f52e3b849a399e736d1526b08b1967
Guilherme Mattos <guilhermevsmattos@outlook.com>
2021-10-02 14:42:29 -0300

    Saving currentUser

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,4 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// ignore: library_prefixes
+import 'package:firebase_auth/firebase_auth.dart' as FireBase;
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:loja_virtual_pro/helpers/firebase_errors.dart';
@@ -6,7 +7,13 @@ import 'package:loja_virtual_pro/helpers/firebase_errors.dart';
 import 'package:loja_virtual_pro/models/user.dart' as User;
 
 class UserManager extends ChangeNotifier {
-  final FirebaseAuth auth = FirebaseAuth.instance;
+  UserManager() {
+    _loadCurrentUser();
+  }
+
+  final FireBase.FirebaseAuth auth = FireBase.FirebaseAuth.instance;
+
+  FireBase.User? user;
 
   bool loading = false;
 
@@ -14,13 +21,15 @@ class UserManager extends ChangeNotifier {
       {User.User? user, Function? onFail, Function? onSuccess}) async {
     setLoading(true);
     try {
-      await auth.signInWithEmailAndPassword(
+      FireBase.UserCredential result = await auth.signInWithEmailAndPassword(
           email: user!.email, password: user.password);
+
+      this.user = result.user;
 
       onSuccess!();
     } on PlatformException catch (e) {
       onFail!(getErrorString(e.code));
-    } on FirebaseAuthException catch (e) {
+    } on FireBase.FirebaseAuthException catch (e) {
       onFail!(getErrorString(e.code));
     }
     setLoading(false);
@@ -28,6 +37,14 @@ class UserManager extends ChangeNotifier {
 
   void setLoading(bool value) {
     loading = value;
+    notifyListeners();
+  }
+
+  Future<void> _loadCurrentUser() async {
+    final FireBase.User? currentUser = await auth.currentUser;
+    if (currentUser != null) {
+      user = currentUser;
+    }
     notifyListeners();
   }
 }
